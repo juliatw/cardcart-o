cae2f24fafac7c7b271a5b5fd39b0f2bd0537f62
juliatw <161319400+juliatw@users.noreply.github.com>
2024-11-11 09:09:15 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('container')
-    let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+  let container = document.getElementById('container')
+  let cartao = document.createElement('article')
+  cartao.className = 'cartao'
 
 
-    cartao.innerHTML = ` 
-      <div class="cartao__conteudo">
-       <h3>${categoria}</h3>
-        <div class="cartao__conteudo__pergunta"> 
+  cartao.innerHTML = ` 
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta"> 
         <p>${pergunta}</p> 
-        </div> 
-        <div class="cartao__conteudo__resposta">
+    </div> 
+    <div class="cartao__conteudo__resposta">
          <p>${pergunta}</p>
-          </div>
-           </div> 
+    </div>
+    </div> 
            `
-    container.appendChild(cartao)
+  container.appendChild(cartao)
 }
